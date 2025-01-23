fcac497d4407148c67bfb7449fc5e767f23e6af3
Cesar Alberto Roman Reyes <cesaralbertoromanreyes@Mac-mini-de-Cesar.local>
2025-01-23 01:14:48 -0600

    [MAIN SCREEN] Project was cleaned for start

---
+++ src/SequencesApp.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function SequencesApp() {
+
+  return (
+    <div className='container'>
+      <h1>Este es mi app</h1>
+      {/*FORM Componente */}
+
+      {/* Result component */}
+      <h2>Resultado:3</h2>
+    </div>
+  )
+}
+
+export default SequencesApp
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import SequencesApp from './SequencesApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SequencesApp />
   </StrictMode>,
 )
