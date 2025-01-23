4c4e5ebc675087e8a7f3dcbf5482a7c74a923326
Cesar Alberto Roman Reyes <cesaralbertoromanreyes@Mac-mini-de-Cesar.local>
2025-01-23 01:16:19 -0600

    [MODEL]SequenceCalculator class was created with methods fibonacci, triangular and primes

---
+++ src/model/SequencesCalculator.ts
@@ -0,0 +1,49 @@
+export class SequencesCalculator {
+
+    private  fibonacci (n:number) {
+        if(n < 1 ) return 0
+
+        let num1 = 0;
+        let num2 = 1;
+        let temp = 0
+    
+        while (n > 0){
+            temp = num2;
+            num2 += num1;
+            num1 = temp;
+    
+            n --;
+        }
+        return num1
+    }
+
+    private triangular (n:number) {
+        return n*(n+1) / 2
+    }
+
+
+    private isPrime  = (value:number) => {
+         if(value< 2) return false;
+
+        for(let i = 2 ; i < value ; i++){
+            if(value % i === 0) return false
+        }
+    
+        return  true
+    } 
+
+    private prime (n:number) {
+        let primes = [2];
+        for(let i = 3 ; primes.length < n ; i++){
+            if(this.isPrime(i)) {
+                primes.push(i);
+            }
+        }
+        return primes[primes.length-1];
+    }
+
+    public calculate (n:number) {
+        return 4*this.prime(n) - this.triangular(n-1) / this.fibonacci(n+2)
+    }
+
+}
