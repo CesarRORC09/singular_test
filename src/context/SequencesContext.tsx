19c557a734e803d38b094abedff842cf75254a21
Cesar Alberto Roman Reyes <cesaralbertoromanreyes@Mac-mini-de-Cesar.local>
2025-01-23 02:04:44 -0600

    [Context] Error param is add to context

--- src/context/SequencesContext.tsx
+++ src/context/SequencesContext.tsx
@@ -3,18 +3,21 @@ import React, { createContext, ReactNode, useContext, useState } from 'react'
 interface SequenceContextType {
     value: string; 
     result: number; 
+    error: string;
     setValue: (value: string) => void; 
     setResult: (result: number) => void; 
+    setError: (error:string) => void;
   }
 
  const SequenceContext = createContext<SequenceContextType | null>(null);
 
 const SequencesProvider:  React.FC<{ children: ReactNode }> = ({children}) => {
     const [value, setValue] = useState<string>(''); 
+    const [error, setError] = useState<string>(''); 
     const [result, setResult] = useState<number>(0); 
 
   return (
-    <SequenceContext.Provider value={{ value, result, setValue, setResult }}>
+    <SequenceContext.Provider value={{ value, result, error ,setValue, setResult, setError }}>
       {children}
     </SequenceContext.Provider>
   );
--- src/viewModel/useFormViewModel.tsx
+++ src/viewModel/useFormViewModel.tsx
@@ -5,7 +5,14 @@ const sequencesCalculator = new SequencesCalculator();
 
 export const useFormViewModel = () => {
 
-    const {result,setResult,setValue,value} = useSequenceContext();
+    const {
+        result,
+        value,
+        error,
+        setResult,
+        setValue,
+        setError,
+    } = useSequenceContext();
 
    const setinput = ({target}:React.ChangeEvent<HTMLInputElement>) => {
         if(/(?:0|[1-9]\d*)/.test(target.value) || target.value === '') return setValue(target.value)
@@ -13,14 +20,17 @@ export const useFormViewModel = () => {
 
 
    const submit = () => {
+    const parsedValue = Number(value);
+    if(isNaN(parsedValue)) return setError('Solo se aceptan valores numericos');
+    if(parsedValue < 0) return setError('Solo se aceptan valores positivos');
     setResult(sequencesCalculator.calculate(Number(value)));
    }
 
   return {
       value,
       result,
+      error,
       submit,
       setinput,
-    
   }
 }
